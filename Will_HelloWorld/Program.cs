82c099121deb9353fa8052f8f511db77fba1b434
Nick Will <ndw1117@rit.edu>
2022-08-24 12:58:36 -0400

    Submission of PE1 HelloWorld which prints my name.

---
+++ Will_HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Will_HelloWorld
+{
+    // Class: Program
+    // Author: Nick Will
+    // Purpose: To demonstrate a basic understanding of output in C#
+    // Restrictions: None
+    internal class Program
+    {
+        // Method: Main
+        // Purpose: To output a basic message
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nick Will");
+        }
+    }
+}
