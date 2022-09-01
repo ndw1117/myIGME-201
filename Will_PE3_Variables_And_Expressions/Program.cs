cf7790c62d99fab3fc1dd342a7fad59a34d78307
Nick Will <ndw1117@rit.edu>
2022-08-31 20:14:17 -0400

    #5 on PE3

---
+++ Will_PE3_Variables_And_Expressions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Will_PE3_Variables_And_Expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 1;
+            int variable;
+
+            for (int i = 0; i < 4; i++)
+            {
+                
+                Console.Write($"Enter int value #{i + 1}: ");
+                variable = Convert.ToInt32(Console.ReadLine());
+                
+                total *= variable;
+                Console.WriteLine("");
+
+            }
+
+            Console.WriteLine($"The product of the numbers is {total}");
+            
+            
+
+        }
+    }
+}
