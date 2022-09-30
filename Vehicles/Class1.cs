9bc91ce79ec6b8305f7aa78600a84ff7c09ec86f
Nick Will <ndw1117@rit.edu>
2022-09-30 18:01:59 -0400

    PE11

---
+++ Vehicles/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+    }
+
+    public class Compact : Car, IPassengerCarrier
+    {
+        public void LoadPassenger()
+        {
+
+        }
+ 
+    }
+
+    public class SUV : Car, IPassengerCarrier
+    {
+        public void LoadPassenger()
+        {
+
+        }
+
+    }
+
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
+        public void LoadPassenger()
+        {
+
+        }
+
+    }
+
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+        public void LoadPassenger()
+        {
+
+        }
+
+    }
+
+    public class FreightTrain : Train, IHeavyLoadCarrier
+    {
+  
+    }
+
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+
+    }
+
+    public abstract class Car : Vehicle
+    {
+
+    }
+
+    public abstract class Train : Vehicle
+    {
+
+    }
+
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
+
+    public interface IHeavyLoadCarrier
+    {
+
+    }
+}
