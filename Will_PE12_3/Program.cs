484e21823f7b04bf71ca6d7b6a6b3cc56e0cf3eb
Nick Will <ndw1117@rit.edu>
2022-10-03 23:58:59 -0400

    PE12 In Progress

---
+++ Will_PE12_3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Will_PE12_3
+{
+    //Class: Program
+    //Author: Nick Will
+    //Purpose: Demonstrates an understanding of classes.
+    //Restrictions: None
+    internal class Program
+    {
+        //Class: MyClass
+        //Purpose: The class to be derived from
+        //Restrictions: None
+        public class MyClass
+        {
+            private string myString;
+
+            public string MyString
+            {
+                set
+                {
+                    myString = value;
+                }
+            }
+
+            public virtual string GetString()
+            {
+                return myString;
+            }
+
+        }
+
+        //Class: MyDerivedClass
+        //Purpose: Derives itself from MyClass and overrides the GetString() method
+        //Restrictions: None
+        public class MyDerivedClass : MyClass
+        {
+            public override string GetString()
+            {
+                return (base.GetString() + " (output from the derived class)");
+            }
+        }
+
+        //Method: Main
+        //Purpose: Instantiates a MyDerivedClass and outputs a string
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            MyDerivedClass myDerivedClass = new MyDerivedClass();
+
+            Console.WriteLine(myDerivedClass.GetString());
+        }
+    }
+}
