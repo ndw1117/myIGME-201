aff261742be4d46065ac7793fad3892146996bd1
Nick Will <ndw1117@rit.edu>
2022-10-14 12:40:27 -0400

    PE 11 #6 Fixed (Traffic File Was Originally Lost)

---
+++ Traffic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Vehicles;
+
+namespace Traffic
+{
+    //Class: Traffic
+    //Author: Nick Will
+    //Purpose: Demonstrates a class which uses the Vehicles library
+    //Restrictions: None
+    class Traffic
+    {
+        static void Main(string[] args)
+        {
+            Compact compact = new Compact();
+            AddPassenger(compact);
+
+            /*
+             * This does not work because freightTrain is not a passenger carrier
+             * FreightTrain freightTrain = new FreightTrain();
+             * AddPassenger(freightTrain);
+            */
+        }
+
+        public static void AddPassenger(IPassengerCarrier vehicleObject)
+        {
+            vehicleObject.LoadPassenger();
+            Console.WriteLine(vehicleObject.ToString());
+            Console.WriteLine("");
+        }
+    }
+}
