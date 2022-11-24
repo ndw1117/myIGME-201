17bd6d63a0cd7765a66bd0c2912cb27435ebbb1e
Nick Will <ndw1117@rit.edu>
2022-11-23 23:03:47 -0500

    PE 24 Tutorial Completed

---
+++ GifFinder/Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GifFinder
+{
+    public partial class SearchForm : Form
+    {
+        public string response;
+        public string searchTerm;
+        public int maxItems;
+        public SearchForm()
+        {
+            InitializeComponent();
+
+            this.maxItemsTextBox.KeyPress += new KeyPressEventHandler(MaxItemsTextBox__KeyPress);
+            this.okayButton.Click += new EventHandler(OkayButton__Click);
+            this.cancelButton.Click += new EventHandler(CancelButton__Click);
+
+
+        }
+
+        private void MaxItemsTextBox__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+
+            if (Char.IsDigit(e.KeyChar) || e.KeyChar == '\b')
+            {
+                e.Handled = false;
+            }
+        }
+
+        private void OkayButton__Click(object sender, EventArgs e)
+        {
+            this.response = "OK";
+
+            this.searchTerm = searchTermTextBox.Text;
+            this.maxItems = Convert.ToInt32(maxItemsTextBox.Text);
+            this.Hide();
+        }
+
+        private void CancelButton__Click(object sender, EventArgs e)
+        {
+            this.response = "Cancel";
+            this.Hide();
+        }
+    }
+}
