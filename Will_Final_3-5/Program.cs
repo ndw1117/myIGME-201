8b6716cc547394fee0d81ddd8ab7c59aa4bb1180
ndw1117 <ndw1117@g.rit.edu>
2022-12-14 18:44:24 -0500

    Final Exam Completed

--- Will_Final_3-5/Program.cs
+++ Will_Final_3-5/Program.cs
@@ -9,6 +9,8 @@ namespace Will_Final_3_5
 {
     internal class Program
     {
+        //An adjacency matrix and list for the digraph on question #3 of the final exam
+
         public static List<Node> colorNodes = new List<Node>();
 
         //An enum to represent the color of the nodes
@@ -85,7 +87,7 @@ namespace Will_Final_3_5
             }
         }
 
-        //Used for the dijkstra shortest path search for question #5 oon the final exam
+        //Used for the dijkstra shortest path search for question #5 on the final exam
         public class Node : IComparable<Node>
         {
             public EColor nState;
@@ -135,8 +137,8 @@ namespace Will_Final_3_5
         {
             DijkstraSearch();
             List<Node> shortestPath = new List<Node>();
-            shortestPath.Add(colorNodes[5]);
-            BuildShortestPath(shortestPath, colorNodes[5]);
+            shortestPath.Add(colorNodes[7]);
+            BuildShortestPath(shortestPath, colorNodes[7]);
             shortestPath.Reverse();
             return (shortestPath);
         }
@@ -160,7 +162,7 @@ namespace Will_Final_3_5
 
         static private void DijkstraSearch()
         {
-            Node start = colorNodes[2];
+            Node start = colorNodes[0];
 
             start.minCostToStart = 0;
             List<Node> prioQueue = new List<Node>();
@@ -174,15 +176,6 @@ namespace Will_Final_3_5
                 // sort our prioQueue by minCostToStart
                 // option #1, use .Sort() which sorts in place
                 prioQueue.Sort();
-
-                // option #2, use .OrderBy() with a delegate method or lambda expression 
-                // the next 6 lines are equivalent from descriptive to abbreviated:
-                prioQueue = prioQueue.OrderBy(nodeOrderBy).ToList();
-                prioQueue = prioQueue.OrderBy(delegate (Node n) { return n.minCostToStart; }).ToList();
-                prioQueue = prioQueue.OrderBy((Node n) => { return n.minCostToStart; }).ToList();
-                prioQueue = prioQueue.OrderBy((n) => { return n.minCostToStart; }).ToList();
-                prioQueue = prioQueue.OrderBy((n) => n.minCostToStart).ToList();
-                prioQueue = prioQueue.OrderBy(n => n.minCostToStart).ToList();
 
                 Node node = prioQueue.First();
                 prioQueue.Remove(node);
@@ -209,20 +202,38 @@ namespace Will_Final_3_5
 
                 node.visited = true;
 
-                // if we reeached our target
-                if (node == colorNodes[5])
+                // if we reached our target
+                if (node == colorNodes[7])
                 {
                     return;
                 }
             } while (prioQueue.Any());
         }
 
+        static public void InitializeEdges()
+        {
+            colorNodes[0].AddEdge(1, colorNodes[1]); //red to yellow
+            colorNodes[0].AddEdge(5, colorNodes[4]); //red to grey
+            colorNodes[1].AddEdge(8, colorNodes[2]); //blue to yellow
+            colorNodes[1].AddEdge(1, colorNodes[3]); //blue to cyan
+            colorNodes[3].AddEdge(1, colorNodes[1]); //cyan to blue
+            colorNodes[4].AddEdge(0, colorNodes[3]); //grey to cyan
+            colorNodes[3].AddEdge(0, colorNodes[4]); //cyan to grey
+            colorNodes[4].AddEdge(1, colorNodes[6]); //grey to orange
+            colorNodes[2].AddEdge(6, colorNodes[7]); //yellow to green
+            colorNodes[5].AddEdge(1, colorNodes[2]); //purple to yellow
+            colorNodes[6].AddEdge(1, colorNodes[5]); //orange to purple
+        }
+
         static void Main(string[] args)
         {
             //DFS Search
+            Console.Write("DFS: ");
             DFS(EColor.red);
 
             //Dijkstra Shortest Path
+            Console.Write("\nDijkstra's: ");
+
             Node node;
 
             for (int i = 0; i < colorAGraph.Length; ++i)
@@ -230,15 +241,17 @@ namespace Will_Final_3_5
                 node = new Node((EColor)i);
                 colorNodes.Add(node);
             }
-           
+
+            InitializeEdges();
+
             List<Node> shortList = GetShortestPathDijkstra();
 
             foreach (Node cNode in shortList)
             {
                 Console.Write(cNode.nState.ToString() + " ");
             }
-            
 
+            Console.WriteLine();
             
         }
     }
